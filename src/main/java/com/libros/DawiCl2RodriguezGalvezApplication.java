5f12c699b1cdeade4bbc601f6b24a6f64055b832
FARG <farg_96@outlook.com>
2023-05-22 16:09:39 -0500

    Creacion del proyecto

---
+++ src/main/java/com/libros/DawiCl2RodriguezGalvezApplication.java
@@ -0,0 +1,13 @@
+package com.libros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiCl2RodriguezGalvezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiCl2RodriguezGalvezApplication.class, args);
+	}
+
+}
